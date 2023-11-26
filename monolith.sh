434e51504efbac5a87bbdc967e2802457829e836
Zelo <30455297+zelodev@users.noreply.github.com>
2023-11-26 23:41:41 +0200

    Add support for legacy Android versions

--- monolith.sh
+++ monolith.sh
@@ -1,11 +1,25 @@
 #!/system/bin/sh
 
-MODE="speed"
+SDK="$(getprop ro.build.version.sdk)"
 
+if [ "$SDK" -ge 34 ]; then
+echo ">>> SDK version is $SDK"
+echo ">>> Using updated method"
+echo
+MODE="speed"
 echo ">>> Compiling"
 echo
 cmd package compile -a -f --full -m "$MODE"
-
+else
+echo ">>> SDK version is $SDK"
+echo ">>> Using legacy method"
+echo
+MODE="everything-profile"
+echo ">>> Compiling"
+echo
+cmd package compile -a -f -m "$MODE"
+cmd package compile -a -f --compile-layouts
+fi
 echo ">>> Cleaning up"
 echo
 cmd package bg-dexopt-job
