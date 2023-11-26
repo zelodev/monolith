61f88d4841d79d5a6f995ff62359ed05e92d248c
Zelo <30455297+zelodev@users.noreply.github.com>
2023-11-26 23:57:07 +0200

    Indent

--- monolith.sh
+++ monolith.sh
@@ -3,22 +3,22 @@
 SDK="$(getprop ro.build.version.sdk)"
 
 if [ "$SDK" -ge 34 ]; then
-echo ">>> SDK version is $SDK"
-echo ">>> Using updated method"
-echo
-MODE="speed"
-echo ">>> Compiling"
-echo
-cmd package compile -a -f --full -m "$MODE"
+  echo ">>> SDK version is $SDK"
+  echo ">>> Using updated method"
+  echo
+  MODE="speed"
+  echo ">>> Compiling"
+  echo
+  cmd package compile -a -f --full -m "$MODE"
 else
-echo ">>> SDK version is $SDK"
-echo ">>> Using legacy method"
-echo
-MODE="everything-profile"
-echo ">>> Compiling"
-echo
-cmd package compile -a -f -m "$MODE"
-cmd package compile -a -f --compile-layouts
+  echo ">>> SDK version is $SDK"
+  echo ">>> Using legacy method"
+  echo
+  MODE="everything-profile"
+  echo ">>> Compiling"
+  echo
+  cmd package compile -a -f -m "$MODE"
+  cmd package compile -a -f --compile-layouts
 fi
 echo ">>> Cleaning up"
 echo
